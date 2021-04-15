17566a3ff61ce4929cd366ecf16fafb962102f21
Patrick Wensing <pwensing@gmail.com>
2021-04-15 09:04:23 -0400

    small updates to generalize ID_derivatives to floating base case

--- spatial/crm.m
+++ spatial/crm.m
@@ -7,7 +7,7 @@ function  vcross = crm( v )
 % value is a 6x6 matrix.  Otherwise, v is taken to be a planar vector, and
 % the return value is 3x3.
 
-if length(v) == 6 && size(v,2) == 1
+if length(v(:)) == 6 && size(v,2) == 1
 
   vcross = [  0    -v(3)  v(2)   0     0     0    ;
 	      v(3)  0    -v(1)   0     0     0    ;
@@ -16,11 +16,13 @@ if length(v) == 6 && size(v,2) == 1
 	      v(6)  0    -v(4)   v(3)  0    -v(1) ;
 	     -v(5)  v(4)  0     -v(2)  v(1)  0 ];
 
-elseif size(v,2) == 1
+elseif length(v(:)) == 3 && size(v,2) == 1
 
   vcross = [  0     0     0    ;
 	      v(3)  0    -v(1) ;
 	     -v(2)  v(1)  0 ];
 else
+    assert(all(size(v)==[6 6]),'Wrong size input to crm'); 
     vcross = [ skew(v(1:3,1:3)) ; skew(v(4:6,1:3))];
+    assert(all(diag(v) == 0),'bad input to crm')
 end

1ce5c55c5cc80720e1d24b2aa948cc6b71d139ba
Patrick Wensing <pwensing@nd.edu>
2023-10-19 10:55:25 -0400

    small upgrade to complex step jacobian

---
+++ v3/utility/complexStepJacobian.m
@@ -0,0 +1,26 @@
+function [J, evals, steps] = complexStepJacobian(f, x, step)
+    if nargin == 2
+        step = eps;
+    end
+    f0 = f(x);
+    f0_vec = f0(:);
+    
+    n = length(f0_vec);
+    m = length(x);
+    
+    J = zeros(n,m);
+    for ind = 1:m
+       e = zeros(m,1);
+       e(ind) = 1;
+       steps(:,ind) = x+1i*e*step;
+       evals(:,ind) = reshape( f(x+1i*e*step), [], 1);
+       
+       J(:,ind) = imag( evals(:,ind) )/step;  
+    end
+    
+    evals = reshape(evals, [size(f0) length(x)]);
+    J     = reshape(J, [size(f0) length(x)]);
+    if size(J,2) == 1
+        J = squeeze(J);
+    end
+end
